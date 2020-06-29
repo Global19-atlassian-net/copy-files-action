eb48b1b1a8c2c7e4e21d3123ef902b0a1bac3184
Marco Schmidt <marcog.schmidt@gmail.com>
2020-06-29 08:55:25 -0400

    Log event payload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,28 @@
-#!/bin/sh -l
+#!/bin/sh
 
 # Suggested by Github actions to be strict
 set -e
 set -o pipefail
 
 if [[ -z "$INPUT_GITHUB_TOKEN" ]]; then
-  echo "Set the GITHUB_TOKEN env variable."
+  echo "Set the GITHUB_TOKEN input."
   exit 1
 fi
-
-echo "token: $INPUT_GITHUB_TOKEN"
 
 git config --global url."https://$INPUT_GITHUB_TOKEN:@github.com/".insteadOf https://github.com/
 
 git config --global user.email "copy-files-action@solo.io"
 git config --global user.name "copy-files-action"
 
-echo "Clone solo-apis..."
+echo "Actor: $GITHUB_ACTOR"
+echo "Workflow: $GITHUB_WORKFLOW"
+echo "Event name: $GITHUB_EVENT_NAME"
+echo "Event path: $GITHUB_EVENT_PATH"
+
+event_payload=$(jq --raw-output . "$GITHUB_EVENT_PATH")
+echo "$event_payload"
+
+echo "Cloning solo-apis repository..."
 git clone https://github.com/solo-io/solo-apis.git
 
 ls solo-apis
