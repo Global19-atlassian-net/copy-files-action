011765c270648dd4f483123ba23ff088ea0c04f0
Marco Schmidt <marcog.schmidt@gmail.com>
2020-06-28 20:29:07 -0400

    git config

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,13 @@
 set -e
 set -o pipefail
 
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
+fi
+
+RUN git config --global url."https://$GITHUB_TOKEN:@github.com/".insteadOf https://github.com/
+
 echo "Clone solo-apis..."
 git clone https://github.com/solo-io/solo-apis.git
 
@@ -13,9 +20,14 @@ cp README.md solo-apis/README.md
 
 cd solo-apis
 
+echo "pwd"
+pwd
+
 git checkout -b test-action
 
 git add .
+
+git status
 
 git commit -m "test"
 
