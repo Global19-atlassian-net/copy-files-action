0f3279fae6dec52297158f680bdafd30ba158337
Marco Schmidt <marcog.schmidt@gmail.com>
2020-06-28 21:37:07 -0400

    Typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,8 @@ echo $INPUT_GITHUB_TOKEN
 
 git config --global url."https://$INPUT_GITHUB_TOKEN:@github.com/".insteadOf https://github.com/
 
-git config --user.email "copy-files-action@solo.io"
-git config --global user.name "copy-files-action"
+git config user.email "copy-files-action@solo.io"
+git config global user.name "copy-files-action"
 
 echo "Clone solo-apis..."
 git clone https://github.com/solo-io/solo-apis.git
