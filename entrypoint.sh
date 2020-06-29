f18af77d6150fa60ac6be0a95343a13440752bd7
Marco Schmidt <marcog.schmidt@gmail.com>
2020-06-28 20:17:53 -0400

    Try to copy file to api repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,22 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+# Suggested by Github actions to be strict
+set -e
+set -o pipefail
+
+echo "Clone solo-apis..."
+git clone https://github.com/solo-io/solo-apis.git
+
+ls solo-apis
+
+cp README.md solo-apis/README.md
+
+cd solo-apis
+
+git checkout -b test-action
+
+git add .
+
+git commit -m "test"
+
+git push -u origin test-action
